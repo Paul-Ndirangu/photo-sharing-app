3d770954b199f9323c98202747fc19e8acd1bcf9
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-02-20 09:46:02 +0300

    update 6 files and create 8 files

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include 
+
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin', admin.sites.urls),
+    path('admin/', admin.site.urls),
     
     # Main app
     path('', include('photoapp.urls')),
--- config/photoapp/models.py
+++ config/photoapp/models.py
@@ -1,7 +1,7 @@
 # photoapp/models.py
 from django.db import models
 from django.contrib.auth import get_user_model
-from taggit.managers import TaaggableManager
+from taggit.managers import TaggableManager
 
 class Photo(models.Model):
     title = models.CharField(max_length=45)
@@ -9,7 +9,7 @@ class Photo(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='photos/')
     submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    tags = TaaggableManager()
+    tags = TaggableManager()
     
     def __str__(self):
         return self.title
