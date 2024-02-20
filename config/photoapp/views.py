2292638e897409dfd01a4e905b0a01d83a255291
Paul-Ndirangu <paulmwaura254@gmail.com>
2024-02-20 22:55:43 +0300

    .

--- config/photoapp/views.py
+++ config/photoapp/views.py
@@ -1,10 +1,8 @@
-# photoapp/views.py
-from django.db.models.query import QuerySet
-from django.shortcuts import render
+'''Photo app generic views'''
 
 from django.shortcuts import get_object_or_404
 
-from django.core.exceptions import PermissionDenied # raise an HTTP 403 exception when called
+from django.core.exceptions import PermissionDenied
 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -14,73 +12,84 @@ from django.urls import reverse_lazy
 
 from .models import Photo
 
-
 class PhotoListView(ListView):
-    model = Photo
+    
+    model = Photo     
+
     template_name = 'photoapp/list.html'
+
     context_object_name = 'photos'
-    
-    
+
+
 class PhotoTagListView(PhotoListView):
+    
     template_name = 'photoapp/taglist.html'
     
-    # Custom method
+    # Custom function
     def get_tag(self):
         return self.kwargs.get('tag')
-    
+
     def get_queryset(self):
-        return self.model.objects.filter(tag__slug=self.get_tag())
+        return self.model.objects.filter(tags__slug=self.get_tag())
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["tag"] = self.get_tag()
-        return context 
+        return context
+     
 
 class PhotoDetailView(DetailView):
+
     model = Photo
+
     template_name = 'photoapp/detail.html'
+
     context_object_name = 'photo'
-    
-    
+
+
 class PhotoCreateView(LoginRequiredMixin, CreateView):
+
     model = Photo
+    
     fields = ['title', 'description', 'image', 'tags']
+
     template_name = 'photoapp/create.html'
+    
     success_url = reverse_lazy('photo:list')
-    
+
     def form_valid(self, form):
+
         form.instance.submitter = self.request.user
+        
         return super().form_valid(form)
-    
-    
-class UserrIsSubmitter(UserPassesTestMixin):
+
+class UserIsSubmitter(UserPassesTestMixin):
+
     # Custom method
     def get_photo(self):
         return get_object_or_404(Photo, pk=self.kwargs.get('pk'))
     
     def test_func(self):
+        
         if self.request.user.is_authenticated:
             return self.request.user == self.get_photo().submitter
         else:
-            raise PermissionDenied("Sorry user not authenticated")
-        
+            raise PermissionDenied('Sorry you are not allowed here')
 
 class PhotoUpdateView(UserIsSubmitter, UpdateView):
-
+    
     template_name = 'photoapp/update.html'
 
     model = Photo
 
     fields = ['title', 'description', 'tags']
-
+    
     success_url = reverse_lazy('photo:list')
 
 class PhotoDeleteView(UserIsSubmitter, DeleteView):
-
+    
     template_name = 'photoapp/delete.html'
 
     model = Photo
 
-    success_url = reverse_lazy('photo:list')  
-    
-    
+    success_url = reverse_lazy('photo:list')
